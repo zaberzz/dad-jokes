7ad8e984a72549ad3494521d8a61baae871e4f30
Saba <saba.sadegh67@gmail.com>
2022-12-22 11:14:35 +0330

    fetch data from dadjoke

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ const url = "https://icanhazdadjoke.com/";
 const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
 
-btn.addEventListener("click", () => {});
+btn.addEventListener("click", () => {
+  fetchDadJoke();
+});
+
+const fetchDadJoke = async () => {
+  const response = await fetch(url, {
+    headers: { Accept: "application/json", "User-Agent": "learning app" },
+  });
+  const data = await response.json();
+  result.textContent = data.joke;
+};
